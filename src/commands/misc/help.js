26b6593f25f6dd6944266b0b9b698e17362e2a42
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2021-10-15 19:11:47 +0200

    add help cmd

---
+++ src/commands/misc/help.js
@@ -0,0 +1,36 @@
+const {Command} = require('discord-akairo')
+const { MessageEmbed } = require('discord.js');
+
+class HelpCommand extends Command {
+    constructor() {
+        super('help', {
+            aliases: ['help']
+        });
+    }
+
+    exec(message){
+        if(message.channel.id === "847434193780604938" && "822108267538677774"){
+            const helpEmbed = new MessageEmbed()
+                .author('Moi-même')
+                .setTitle("Panneau des commandes")
+                .setDescription("Ici se trouve toutes les commandes du bot")
+                .addFields({
+                    name: "!who", value: "Qui est-ce ?"
+                },
+                {
+                    name: "!secrets", value: "Action ou Vérité ?"
+                },
+                {
+                    name: "!jnj", value: "Je n'ai jamais ?"
+                })
+                .setFooter("Bonne chance !")
+
+                message.channel.send({embeds: [helpEmbed]})
+                
+        }else{
+
+        }
+    }
+}
+
+module.exports = HelpCommand
