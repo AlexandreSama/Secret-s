16fb5cc257162d6991c47c609ac980b2716b7ac4
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2021-10-18 13:34:16 +0200

    i hate this..

--- src/commands/admin/reload.js
+++ src/commands/admin/reload.js
@@ -11,7 +11,7 @@ class ReloadCommand extends Command {
 
         if(message.author.id === "256892994504884224"){
             await message.delete()
-            await message.guild.cache.get('899616869177258074').send("Je cherche du café...")
+            await message.guild.channels.cache.get('899616869177258074').send("Je cherche du café...")
             process.exit()
         }else{
             await message.channel.send("Qui est-tu pour me donner des ordres ?")
