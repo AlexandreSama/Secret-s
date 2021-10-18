28823af7a4f3ef8fb5c82e76d67c3125c3981cde
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2021-10-18 13:20:04 +0200

    fix permissions

--- src/commands/admin/reload.js
+++ src/commands/admin/reload.js
@@ -2,15 +2,20 @@ const {Command} = require('discord-akairo')
 
 class ReloadCommand extends Command {
     constructor() {
-        super('pong', {
-            aliases: ['pong']
+        super('reload', {
+            aliases: ['reload']
         });
     }
 
     async exec(message){
-        await message.delete()
-        await message.channel.send("Je cherche du café...")
-        process.exit()
+
+        if(message.author.id === "256892994504884224"){
+            await message.delete()
+            await message.channel.send("Je cherche du café...")
+            process.exit()
+        }else{
+            await message.channel.send("Qui est-tu pour me donner des ordres ?")
+        }
     }
 }
 
