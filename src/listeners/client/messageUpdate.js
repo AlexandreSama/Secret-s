16aa4db96e60fe9a6bed447f0798be49cb3bb133
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2021-10-18 19:03:44 +0200

    add event

---
+++ src/listeners/client/messageUpdate.js
@@ -0,0 +1,28 @@
+const {Listener} = require('discord-akairo')
+const { MessageEmbed } = require('discord.js');
+
+class MessageUpdateListener extends Listener {
+    constructor(){
+        super('messageUpdate', {
+            emitter: 'client',
+            event: 'messageUpdate'
+        });
+    }
+
+    async exec(oldMessage, newMessage) {
+
+        const deletedMessageEmbed = new MessageEmbed()
+        .setAuthor('Secret\'s')
+        .setFooter('Toujours la pour servir dans l\'ombre..')
+    
+        deletedMessageEmbed.setDescription(`Message modifié de ${oldMessage.author.username}`)
+        deletedMessageEmbed.addField("Voici le message original :", oldMessage.content)
+        deletedMessageEmbed.addField('Et le message modifié :', newMessage.content)
+        deletedMessageEmbed.addField('Et il a été modifié dans le channel :', oldMessage.channel.name)
+
+        message.guild.channels.cache.get("899624162837008394").send({embeds: [deletedMessageEmbed]})
+        
+    }
+}
+
+module.exports = MessageUpdateListener
