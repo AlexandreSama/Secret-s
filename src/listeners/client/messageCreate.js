fadfc7748c33b0f7d46809d46d3bfd79eab1cfe9
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2022-03-10 12:43:31 +0100

    refactor code

--- src/listeners/client/messageCreate.js
+++ src/listeners/client/messageCreate.js
@@ -14,35 +14,35 @@ class messageCreateListener extends Listener {
 
     exec(message) {
 
-        if(message.channel.id === "822108267538677774"){
+        if (message.channel.id === "822108267538677774") {
 
             let welcomeDay = ['Bonjour Secret\'s', "bonjour secrets", "Bonjour secret's", "bonjour Secret's"]
-        let welcomeNight = ['Bonsoir Secret\'s', "bonsoir secrets", "Bonsoir secret's", "bonsoir Secret's"]
-        welcomeDay.forEach(element => {
-            if(message.content == element){
-                message.reply('Bonjour mon cher petit esclave :smiling_imp:')
+            let welcomeNight = ['Bonsoir Secret\'s', "bonsoir secrets", "Bonsoir secret's", "bonsoir Secret's"]
+            welcomeDay.forEach(element => {
+                if (message.content == element) {
+                    message.reply('Bonjour mon cher petit esclave :smiling_imp:')
+                }
+            })
+            welcomeNight.forEach(element => {
+                if (message.content == element) {
+                    message.reply('Bonsoir mon cher petit esclave :smiling_imp:')
+                }
+            })
+            if (message.content.includes("Dixie")) {
+                message.reply('Ah ? On parle de la plus belle des salopes de Chicago ?')
             }
-        })
-        welcomeNight.forEach(element => {
-            if(message.content == element){
-                message.reply('Bonsoir mon cher petit esclave :smiling_imp:')
+            if (message.content.includes("Chocolatine") || message.content.includes("chocolatine")) {
+                message.reply('Bande de sauvages !')
             }
-        })
-        if(message.content.includes("Dixie")){
-            message.reply('Ah ? On parle de la plus belle des salopes de Chicago ?')
-        }
-        if(message.content.includes("Chocolatine") || message.content.includes("chocolatine")){
-            message.reply('Bande de sauvages !')
-        }
-        if(message.content.includes("Loup") || message.content.includes("loup")){
-            message.reply('On parle de l\'Alpha des Alphas : Adrian ?')
-        }
-        if(message.content.includes("Ou est Conrad") || message.content.includes("ou est Conrad") || message.content.includes("ou est Conrad ?") || message.content.includes("Ou est Conrad ?")){
-            message.reply('Actuellement ? Dans ton cul :thinking:')
-        }
-        if(message.content.includes("Ou est Mal ?") || message.content.includes("ou est Mal ?") || message.content.includes("Ou est Mal") || message.content.includes("ou est Mal")){
-            message.reply('Actuellement ? Disparu dans les méandres des abdos de Jason Statham :smiling_imp:')
-        }
+            if (message.content.includes("Loup") || message.content.includes("loup")) {
+                message.reply('On parle de l\'Alpha des Alphas : Adrian ?')
+            }
+            if (message.content.includes("Ou est Conrad") || message.content.includes("ou est Conrad") || message.content.includes("ou est Conrad ?") || message.content.includes("Ou est Conrad ?")) {
+                message.reply('Actuellement ? Dans ton cul :thinking:')
+            }
+            if (message.content.includes("Ou est Mal ?") || message.content.includes("ou est Mal ?") || message.content.includes("Ou est Mal") || message.content.includes("ou est Mal")) {
+                message.reply('Actuellement ? Disparu dans les méandres des abdos de Jason Statham :smiling_imp:')
+            }
 
         }
 
@@ -57,8 +57,7 @@ class messageCreateListener extends Listener {
                     name: "Message :",
                     value: message.content,
                     inline: true
-                },
-                {
+                }, {
                     name: "Dans le salon :",
                     value: `<#${message.channel.id}>`,
                     inline: false
@@ -72,10 +71,13 @@ class messageCreateListener extends Listener {
 
         }
 
-        if(message.content === "Qui est la maitresse d'Alexandre ?"){
+        if (message.content === "Qui est la maitresse d'Alexandre ?") {
             message.reply('Ainsi, tu souhaite découvrir l\'identité de la maitresse de mon créateur ? \n Ainsi soit-il !')
             const attachment = new Discord.MessageAttachment("src/AydenQueen.jpg")
-            message.channel.send({content: "Voici la maitresse de mon diabolique maitre !", files: [attachment]})
+            message.channel.send({
+                content: "Voici la maitresse de mon diabolique maitre !",
+                files: [attachment]
+            })
             message.channel.send('Essaye de lui arriver a la cheville (Ou apporte des gaufres)')
         }
     }
